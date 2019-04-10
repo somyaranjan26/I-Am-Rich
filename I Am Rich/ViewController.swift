d47d47f1e96d6456edc7f3b7e13e8634a0312e0b
Somyaranjan <somyaranjanmac1@gmail.com>
2019-04-10 15:24:40 +0530

    Added Icons And App files

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Somyaranjan Rout on 03/04/19.
+//  Copyright © 2019 Somyaranjan Rout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
